8faa157e147a8bd825c3ea5a892a4fe56b1e39fb
arzenshii <arseny.shi@gmail.com>
2026-01-30 17:19:35 +0300

    vrs_3

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -9,9 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.springframework.context.annotation.ComponentScan;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan("web")
 public class WebConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
